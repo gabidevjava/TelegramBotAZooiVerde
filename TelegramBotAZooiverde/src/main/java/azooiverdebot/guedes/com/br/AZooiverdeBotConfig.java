862e5ab5c13afceb5ad5a6ad50504d98d0d7313c
Gabriela Guedes <zooiv3rde@gmail.com>
2019-04-10 21:52:45 -0300

    class initializer bot

--- TelegramBotAZooiverde/src/main/java/azooiverdebot/guedes/com/br/AZooiverdeBotConfig.java
+++ TelegramBotAZooiverde/src/main/java/azooiverdebot/guedes/com/br/AZooiverdeBotConfig.java
@@ -1,7 +1,9 @@
 package azooiverdebot.guedes.com.br;
 
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 public class AZooiverdeBotConfig extends TelegramLongPollingBot {
 	
@@ -13,7 +15,26 @@ public class AZooiverdeBotConfig extends TelegramLongPollingBot {
 	}
 
 	public void onUpdateReceived(Update update) {
-		// TODO Auto-generated method stub
+		
+		String command = update.getMessage().getText();
+		SendMessage message = new SendMessage();
+		
+		if(command.equals("/start")) {
+			message.setText("Muito bem vindo nessa poha " + update.getMessage().getFrom().getFirstName());
+		}
+		
+		if(command.equals("/meunome@azooiverde_bot")) {
+			System.out.println(update.getMessage().getFrom().getFirstName());
+			message.setText("Mas que bosta de nome hein jovem, pqp " + update.getMessage().getFrom().getFirstName());
+		}
+		
+		message.setChatId(update.getMessage().getChatId());
+		
+		try {
+			execute(message);
+		} catch (TelegramApiException e) {
+			e.printStackTrace();
+		}
 		
 	}
 
